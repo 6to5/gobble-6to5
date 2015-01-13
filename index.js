25127e31b31693363de91fa26fd3166b6f3ae834
Chris Reeves <chris@ev-soft.net>
2015-01-13 17:36:35 +1100

    keep 6to5 from dying if it is run after es6-transpiler

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ module.exports = to5;
 
 function to5 ( code, options ) {
 	options.sourceMap = options.sourceMap !== false;
+
+	// 6to5 falls over if someone slips a contains method on the Array prototype (es6-transpiler)
+	if ( Array.prototype.hasOwnProperty( 'contains' ) ) {
+		delete Array.prototype.contains;
+	}
+
 	return require( '6to5-core' ).transform( code, options );
 }
 
