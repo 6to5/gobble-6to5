ca417c540051a84fddb420d985458fbb5bf510a7
Luke Scott <luke@webconnex.com>
2015-04-24 07:35:16 -0700

    Transform babelHelpers so spec.ProtoToAssign works

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var babelc = require('babel-core');
+var resolveRc = require('babel-core/lib/babel/tools/resolve-rc')
 var sander = require('sander');
 var mapSeries = require('promise-map-series');
 var objectAssign = require('object-assign');
@@ -7,39 +8,33 @@ var path = require('path');
 module.exports = babel;
 
 function babel(inputdir, outputdir, opts) {
-	if (opts.importHelpers) {
+	importHelpers = !!opts.importHelpers;
+	if (importHelpers) {
 		opts.externalHelpers = true;
 		opts.metadataUsedHelpers = true;
 	}
+	delete opts.importHelpers;
+	resolveRc(inputdir, opts);
 	return sander.lsr(inputdir).then(function(allFiles) {
 		allFiles = allFiles.filter(extFilter);
 		var usedHelpers = [];
 		return mapSeries(allFiles, function(filename) {
 			return sander.readFile(inputdir, filename).then(function(code) {
-				if (opts.importHelpers) {
+				if (importHelpers) {
 					var hf = slashes(filename) + '__babelHelpers.js';
 					code = 'import {babelHelpers} from "' + hf + '";\n' + code;
 				}
-				var fileOpts = objectAssign({}, opts, {
-					filename: inputdir + '/' + filename,
-					filenameRelative: filename
-				});
-				delete fileOpts.importHelpers;
-				var result = babelc.transform(code, fileOpts);
-				if (opts.importHelpers) {
-					var helpers = result.metadata.usedHelpers;
-					for (var i = helpers.length - 1; i >= 0; i--) {
-						if (usedHelpers.indexOf(helpers[i]) < 0) {
-							usedHelpers.push(helpers[i]);
-						}
-					}
+				var result = babelc.transform(code, objectAssign({}, opts, {
+					filename: inputdir + '/' + filename
+				}));
+				if (importHelpers) {
+					addUniqueHelpers(usedHelpers, result.metadata.usedHelpers);
 				}
 				return sander.writeFile(outputdir, filename, result.code);
 			});
 		}).then(function() {
-			if (opts.importHelpers) {
-				var code = babelc.buildExternalHelpers(usedHelpers, 'var');
-				code = "export " + code;
+			if (importHelpers) {
+				var code = buildHelpers(usedHelpers, opts);
 				return sander.writeFile(outputdir, '__babelHelpers.js', code);
 			}
 		});
@@ -53,6 +48,27 @@ function extFilter(filename) {
 function slashes(from) {
 	var count = from.split('/').length;
 	return count > 1 ? (new Array(count + 1)).join('../') : './';
+}
+
+function buildHelpers(usedHelpers, opts) {
+	var helpers1 = babelc.buildExternalHelpers(usedHelpers, "var");
+	addUniqueHelpers(usedHelpers,
+		babelc.transform(helpers1, objectAssign({}, opts, {
+			externalHelpers: true,
+			metadataUsedHelpers: true
+		})).metadata.usedHelpers
+	);
+	delete opts.metadataUsedHelpers;
+	var helpers2 = babelc.buildExternalHelpers(usedHelpers, "var");
+	return "export " + babelc.transform(helpers2, opts).code;
+}
+
+function addUniqueHelpers(dest, src) {
+	for (var i = src.length - 1; i >= 0; i--) {
+		if (dest.indexOf(src[i]) < 0) {
+			dest.push(src[i]);
+		}
+	}
 }
 
 babel.defaults = {
